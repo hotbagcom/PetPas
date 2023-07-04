2d240668cde7d448e01b02246a81000346a7e329
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2023-07-04 17:18:00 +0300

    Update addpet.dart

--- pet_track_app/lib/addpet.dart
+++ pet_track_app/lib/addpet.dart
@@ -1,11 +1,22 @@
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'sign_upcontroller.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'date_picker_example.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+   MyApp({Key? key});
+
+  SignUpController signUpController = Get.put(SignUpController());
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +31,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
@@ -27,11 +39,42 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+ImagePicker imagePicker =ImagePicker();
 
-class _MyHomePageState extends State<MyHomePage> {
+
+class _MyHomePageState  extends State<MyHomePage> {
+
+
 
   @override
   Widget build(BuildContext context) {
+
+    var petname;
+    void set_petname(String a){
+      petname = a;
+    }
+    var petfamily;
+    void set_petfamily(String b){
+      petfamily = b;
+    }
+    var pettype;
+    void set_pettype(String c){
+      pettype = c;
+    }
+    var petweight;
+    void set_petweight(String d){
+      petweight = d;
+    }
+    var petkode;
+    void set_petkode(String e){
+      petkode = e;
+    }
+    var petbirth;
+    void set_petbirth(String e){
+      petbirth = e;
+    }
+
+
 
     return Scaffold(
       appBar: AppBar(
@@ -39,67 +82,163 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+            ),
             Positioned(
               bottom: 0,
-              child: InkWell(
-                child: Icon(Icons.camera),
-                onTap: (){
-                  print("Camera clicked");
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) => bottomSheet(context),
-                  );
-                },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                  height: 100,
+                  width: 100,
+                  child:Column(
+                    children: [
+                      InkWell(
+                        child: SizedBox(
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 50, // set the size of the circle avatar
+                                backgroundImage: AssetImage(pickedFile.path),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: (){
+                          print("Camera clicked");
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) => bottomSheet(context),
+                          );
+                        },
+                      ),
+
+                    ],
+                  ),
+                ),
+
+                ],
               ),
             ),
             //fotograf ekle
             Padding(
-            padding: const EdgeInsets.only(top: 120.0),
-
+            padding: const EdgeInsets.only(top: 20.0),
             ),//verileri al textfield
-            TextField( //ad
-              onChanged: (value) {
-                setState(() {
 
-                });
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //ad
+                  onChanged: (_a) {
+                    set_petname(_a);
+                  },
+                  decoration: InputDecoration(
+                  labelText : "Adı",
+                  border: OutlineInputBorder(),
+                  ),
+
+                ),
+              ),
             ),
-            TextField( //tür ,birkaç tür koyup diğerleri bölümünü ekleyebilirsin
-              onChanged: (value) {
-                setState(() {
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0 , vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //tür
+                  onChanged: (_b) {
+                    set_petfamily(_b);
+                  },
+                  decoration: InputDecoration(
+                    labelText : "Türü",
+                    border: OutlineInputBorder(),
+                  ),
 
-                });
-              },
+                ),
+              ),
             ),
-            TextField( //cins ,türün birkaç cinsini yaz
-              onChanged: (value) {
-                setState(() {
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0 , vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //ırk 
+                  onChanged: (_c) {
+                    set_pettype(_c);
+                  },
+                  decoration: InputDecoration(
+                    labelText : "ırkı ",
+                    border: OutlineInputBorder(),
+                  ),
 
-                });
-              },
+                ),
+              ),
             ),
-            TextField( //doğum tarihi
-              onChanged: (value) {
-                setState(() {
-
-                });
+            //date.dart ı buraya çağır
+            /*ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DatePickerExample()),
+                );
               },
+              child: Text('$pettype doğum tarihi seç'),
+            ),*/
+            Padding( //doğum tarihi
+              padding: const EdgeInsets.symmetric(horizontal: 20.0 , vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //tür
+                  onChanged: (_f) {
+                    set_petbirth(_f);
+                  },
+                  decoration: InputDecoration(
+                    labelText : "Doğum tarihi",
+                    border: OutlineInputBorder(),
+                  ),
+
+                ),
+              ),
             ),
-            TextField( //kilo
-              onChanged: (value) {
-                setState(() {
 
-                });
-              },
+
+            Padding(//kilo
+              padding: const EdgeInsets.symmetric(horizontal: 20.0 , vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //tür
+                  onChanged: (_d) {
+                    set_petweight(_d);
+                  },
+                  decoration: InputDecoration(
+                    labelText : "Kilosu",
+                    border: OutlineInputBorder(),
+                  ),
+
+                ),
+              ),
             ),
-            TextField( //chip numarası
-              onChanged: (value) {
-                setState(() {
 
-                });
-              },
+
+            Padding(//chip numarası
+              padding: const EdgeInsets.symmetric(horizontal: 20.0 , vertical: 8),
+              child: SizedBox(
+                height: 35,
+                child: TextFormField( //tür
+                  onChanged: (_e) {
+                    set_petkode(_e);
+                  },
+                  decoration: InputDecoration(
+                    labelText : "Çip kodu",
+                    border: OutlineInputBorder(),
+                  ),
+
+                ),
+              ),
             ),
 
 
@@ -109,7 +248,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: Align(
         alignment: Alignment.bottomCenter,
         child: Padding(
-          padding: const EdgeInsets.only(bottom: 20.0),
+          padding: const EdgeInsets.only(bottom: 15.0 ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -171,7 +310,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   onTap: () {
-                    //takePhoto(ImageSource.gallery);
+                    takePhoto(ImageSource.gallery);
                   },
                 ),
 
@@ -193,7 +332,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   onTap: () {
-                    //takePhoto(ImageSource.gallery);
+                    takePhoto(ImageSource.camera);
                   },
                 ),
 
@@ -205,4 +344,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
     );
   }
+
+
+
+}
+Future<void> takePhoto(ImageSource source) async {
+
+  File pickedFile;
+  SignUpController signUpController = Get.find();
+  final pickedImage =
+  await imagePicker.pickImage(source: source, imageQuality: 100);
+
+  pickedFile = File(pickedImage!.path);
+  signUpController.setProfileImagePath(pickedFile.path);
+
+  Get.back();
+
+  // print(pickedFile);
 }
